4bf03c8081ab6e7a34e4f0409d7499e251ae2afe
Yasushi Shimizu <kaizougoo@gmail.com>
2020-09-04 01:18:31 +0900

    [update] like処理途中

--- public/js/script.js
+++ public/js/script.js
@@ -5,12 +5,29 @@ $(() => {
 		var kv = pair[i].split('=');
 		arg[kv[0]]=kv[1];
 	}
-	var m = 60*60  // 秒で指定
-	document.cookie = "id=" + arg['user'] + ";max-age=" + m + ";domain=https://artful.jp/staging-menu/;path=/";
+	$.cookie("id", arg['user'], {path:"/"});
 	let like = document.getElementsByClassName('like');
 	for(let i = 0; i < like.length; i++){
 		like[i].addEventListener('click', () => {
 			like[i].classList.toggle('active');
+			let name = like[i].name;
+			let likes = $.cookie("likes");
+			let likes_sp = likes.split(';');
+			let flag = false;
+			for(let i = 0; i < likes_sp.length; i++){
+				if(likes_sp[i] == name){
+					flag = true;
+					break;
+				}
+			}
+			if(flag){
+				likes_sp.splice(i, 1);
+			}
+			else{
+				likes_sp.push(name);
+			}
+			likes = likes_sp.join(';');
+			$.cookie("likes", likes, {path:"/"});
 		});
 	}
 });
