aa29ebc2b5a7bb035585566e5f005cc97461fbd7
kusakatakashi <tubomi10x29@gmail.com>
2020-09-08 06:01:47 +0900

    [update]historyパスの追加

--- control/config.php
+++ control/config.php
@@ -1,5 +1,13 @@
 <?php
 $root_path = 'https://artful.jp/staging-menu';
-$public_path = $root_path . '/public';
-$control_path = $root_path . '/control';
+$public_path = $root_path . '/public/';
+$control_path = $root_path . '/control/';
+$control_css_path = $root_path . '/control/css/';
 $img_folder_path = $root_path . '/control/img/';
+$signup_path = $root_path . '/control/signup/';
+$login_path = $root_path . '/control/login/';
+$logout_path = $root_path . '/control/logout/';
+$update_store_path = $root_path . '/control/update_store/';
+$update_menu_path = $root_path . '/control/update_menu/';
+$update_rule_path = $root_path . '/control/update_rule/';
+$history_path = $root_path . '/control/history/';
