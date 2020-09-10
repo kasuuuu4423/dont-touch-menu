2b757928ca2c3eaef4b67ca8be51ab48e1ab8662
Yasushi Shimizu <kaizougoo@gmail.com>
2020-09-10 16:32:34 +0900

    [fix] スタイル調整

--- control/update_rule/insert.php
+++ control/update_rule/insert.php
@@ -1,38 +1,42 @@
 <?php
 require  '../pdo/lib_pdo.php';
-session_start();
 $pdo = new Lib_pdo();
 
 require '../header.php';
 ?>
-<?php if($_GET['target'] == 'cat'): ?>
-<h2>ルールカテゴリーの追加</h2>
-<form action="insert.php" method="post">
-  <h2>カテゴリー名</h2>
-  <input type="text" name="rule_cat_name">
-  <input type="submit" name="insert_cat" value="ルールカテゴリーを追加">
-</form>
-<?php endif; ?>
-
-<?php if($_GET['target'] == 'rule'): ?>
-<h2>ルールの追加</h2>
-<form action="insert.php" method="post">
-  <h2>内容</h2>
-  <input type="text" name="rule_content">
-  <input type="hidden" name="rule_cat_id" value="<?php echo $_GET['cat_id']?>">
-  <input type="submit" name="insert_rule" value="ルールを追加">
-</form>
-<?php endif; ?>
-
+<main class="container">
+  <div class="row">
+    <div class="col-12 bar"></div>
+    <?php if($_GET['target'] == 'cat'): ?>
+    <h2 class="col-12">ルールカテゴリーの追加</h2>
+    <form class="col-12 mt-4 text-center" action="insert.php" method="post">
+      <div class="mb-4">
+        <h2>カテゴリー名</h2>
+        <input class="w-100" type="text" name="rule_cat_name">
+      </div>
+      <input class="btn btn-blue" type="submit" name="insert_cat" value="ルールカテゴリーを追加">
+    </form>
+    <?php elseif($_GET['target'] == 'rule'): ?>
+    <h2 class="col-12">ルールの追加</h2>
+    <form class="col-12 mt-4 text-center" action="insert.php" method="post">
+      <div class="mb-4">
+        <h2>内容</h2>
+        <input class="w-100" type="text" name="rule_content">
+      </div>
+      <input type="hidden" name="rule_cat_id" value="<?php echo $_GET['cat_id']?>">
+      <input class="btn btn-blue" type="submit" name="insert_rule" value="ルールを追加">
+    </form>
+    <?php endif; ?>
+  </div>
+</main>
 <?php 
 if(isset($_POST['insert_cat'])){
   $pdo->insert_rule_category($_POST['rule_cat_name'], $_SESSION['ID']);
-  echo '<p><a href="index.php">ルール変更画面へ戻る</a></p>';
-  echo '<p><a href="../index.php">管理画面TOPへ</a></p>';
+  $_SESSION['rule_msg'] = 'カテゴリーを追加しました。';
+  header('Location: ./');
 }
 if(isset($_POST['insert_rule'])){
   $pdo->insert_rule($_POST['rule_content'], $_POST['rule_cat_id'], $_SESSION['ID']);
-  echo 'ルールの追加が完了しました';
-  echo '<p><a href="index.php">ルール変更画面へ戻る</a></p>';
-  echo '<p><a href="../index.php">管理画面TOPへ</a></p>';
+  $_SESSION['rule_msg'] = 'ルールを追加しました。';
+  header('Location: ./');
 }
--- control/update_rule/update.php
+++ control/update_rule/update.php
@@ -1,62 +1,65 @@
 <?php
 require '../pdo/lib_pdo.php';
-session_start();
+require '../header.php';
 $pdo = new Lib_pdo();
 
-if (isset($_SESSION["USERID"])) {
-  echo "ようこそ".($_SESSION["USERID"])."さん<br>";
-  echo "<div><a href='../logout/index.php'></div>ログアウトはこちら</a>";
-  if(isset($_GET['cat_id'])){
+if (isset($_SESSION["USERID"])):
+?>
+
+<main class="container">
+  <div class="row">
+    <div class="col-12 bar"></div>
+<?php
+  if(isset($_GET['cat_id'])):
   $rule_cat = $pdo->select_rule_cat_id($_GET['cat_id'])[0];
 
   $rule_cat_id = $rule_cat['id'];
   $rule_cat_name = $rule_cat['name'];
 ?>
-
-<form action="update.php" method="post">
-  <input type="hidden" name="rule_cat_id" value="<?php if (!empty($rule_cat_id)) echo(htmlspecialchars($rule_cat_id, ENT_QUOTES, 'UTF-8'));?>">
-  <h2>カテゴリー名</h2>
-  <input type="text" name="rule_cat_name" value="<?php if (!empty($rule_cat_name)) echo(htmlspecialchars($rule_cat_name, ENT_QUOTES, 'UTF-8'));?>">
-  <input type="submit" name="confirm_cat" value="編集を確定">
-</form>
-<a href="delete.php?rule_cat_id=<?php echo $rule_cat_id; ?>">このカテゴリーを削除する</a>
-<?php
-  }
-  if(isset($_GET['rule_id'])){
-    $rule = $pdo->select_rule_id($_GET['rule_id'])[0];
-  
-    $rule_id = $rule['id'];
-    $rule_content = $rule['content'];
-  ?>
-  
-  <form action="update.php" method="post">
-    <input type="hidden" name="rule_id" value="<?php if (!empty($rule_id)) echo(htmlspecialchars($rule_id, ENT_QUOTES, 'UTF-8'));?>">
-    <h2>ルール名</h2>
-    <input type="text" name="rule_content" value="<?php if (!empty($rule_content)) echo(htmlspecialchars($rule_content, ENT_QUOTES, 'UTF-8'));?>">
-    <input type="submit" name="confirm_rule" value="編集を確定">
-  </form>
-  <a href="delete.php?rule_id=<?php echo $_GET['rule_id']; ?>">このルールを削除する</a>
-  <?php 
-  }
-}
-?>
-
+    <form class="col-12 mt-12 text-center" action="update.php" method="post">
+      <input type="hidden" name="rule_cat_id" value="<?php if (!empty($rule_cat_id)) echo(htmlspecialchars($rule_cat_id, ENT_QUOTES, 'UTF-8'));?>">
+      <div class="mb-4">
+        <h2>カテゴリー名</h2>
+        <input class="w-100" type="text" name="rule_cat_name" value="<?php if (!empty($rule_cat_name)) echo(htmlspecialchars($rule_cat_name, ENT_QUOTES, 'UTF-8'));?>">
+      </div>
+      <input type="submit" name="confirm_cat" value="編集を確定">
+    </form>
+    <a href="delete.php?rule_cat_id=<?php echo $rule_cat_id; ?>">このカテゴリーを削除する</a>
+    <?php
+      elseif(isset($_GET['rule_id'])):
+        $rule = $pdo->select_rule_id($_GET['rule_id'])[0];
+      
+        $rule_id = $rule['id'];
+        $rule_content = $rule['content'];
+      ?>     
+      <form class="col-12 mt-12 text-center" action="update.php" method="post">
+        <input type="hidden" name="rule_id" value="<?php if (!empty($rule_id)) echo(htmlspecialchars($rule_id, ENT_QUOTES, 'UTF-8'));?>">
+        <div class="mb-4">
+          <h2>ルール名</h2>
+          <input class="w-100" type="text" name="rule_content" value="<?php if (!empty($rule_content)) echo(htmlspecialchars($rule_content, ENT_QUOTES, 'UTF-8'));?>">
+        </div>
+        <input class="btn btn-green" type="submit" name="confirm_rule" value="編集を確定">
+      </form>
+      <a class="btn btn-red mt-4" href="delete.php?rule_id=<?php echo $_GET['rule_id']; ?>">このルールを削除する</a>
+      <?php 
+      endif;
+    endif;
+    ?>
+  </div>
+</main>
 <?php
 
 if(isset($_POST['confirm_cat'])){
   $pdo->update_rule_cat($_POST['rule_cat_id'], $_POST['rule_cat_name']);
-  echo "情報を更新しました。";
+  $_SESSION['rule_msg'] = 'カテゴリーを変更しました。';
+  header('Location: ./');
 }
 
 if(isset($_POST['confirm_rule'])){
   $pdo->update_rule($_POST['rule_id'], $_POST['rule_content']);
-  echo "情報を更新しました。";
+  $_SESSION['rule_msg'] = 'ルールを変更しました。';
+  header('Location: ./');
 }
-?>
 
-<p>
-    <a href="index.php">ルール変更画面へ戻る</a>
-</p>        
-<p>
-    <a href="../index.php">管理画面TOPへ</a>
-</p>
+require '../footer.php';
+?>
