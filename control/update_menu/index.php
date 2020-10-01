c508d9ec3d2c70464b40633228abd927cb74d6c0
kusakatakashi <tubomi10x29@gmail.com>
2020-10-01 14:50:35 +0900

    [update]enabledの変更をajaxで実装

--- control/update_menu/index.php
+++ control/update_menu/index.php
@@ -1,7 +1,8 @@
 <?php
 
-require '../header.php';
-require '../pdo/lib_pdo.php';
+require '../../config.php';
+require '../elements/header.php';
+require '../../lib/pdo/lib_pdo.php';
 
 if (isset($_SESSION["USERID"])):
   if(isset($_SESSION['menu_msg'])){
@@ -28,6 +29,7 @@ if (isset($_SESSION["USERID"])):
         $row_menu[$index]['id'] = $row['id'];
         $row_menu[$index]['name'] = $row['name'];
         $row_menu[$index]['enabled'] = $row['enabled'];
+        $row_menu[$index]['likes'] = $row['likes'];
         $row_menu[$index]['menu_category_id'] = $row['menu_category_id'];
       }
 
@@ -50,20 +52,24 @@ if (isset($_SESSION["USERID"])):
                 $flag = false;
                 if(is_array($row_menu)):
                   ?>
-                  <div class="col-12 tbl tbl_update_index">
+                  <div class="col-12 tbl tbl_update_index update_menu_index">
                     <div class="row">
                       <?php
                       foreach($row_menu as $value_menu):
                         if($value_menu['menu_category_id'] == $value_cat['id']):
                           ?>
-                          <div class="col-12 tbl_row">
+                          <div class="col-12 tbl_row" id="<?php echo $value_menu['id']; ?>">
                             <div class="row">
-                              <div class="col-5 item_name"><?php echo $value_menu['name']; ?></div>
-                              <?php if($value_menu['enabled'] == 1): ?>
-                              <div class="col-3 enable">販売中</div>
-                              <?php else: ?>
-                              <div class="col-3 disable">販売停止中</div>
-                              <?php endif; ?>
+                              <div class="col-8 item_name"><?php echo $value_menu['name']; ?></div>
+                              <div class="col-4 likes">♡<?php echo $value_menu['likes']; ?></div>
+                              <div class="col-8 item_enabled btn-group btn-group-toggle" data-toggle="buttons">
+                                <label class="btn btn-primary active">
+                                  <input type="radio" name="radio<?php echo $value_menu['id']; ?>" id="enable" autocomplete="off" <?php  if($value_menu['enabled'] == 1){ echo 'checked'; }?>>販売中
+                                </label>
+                                <label class="btn btn-primary">
+                                  <input type="radio" name="radio<?php echo $value_menu['id']; ?>" id="disable" autocomplete="off" <?php  if($value_menu['enabled'] == 0){ echo 'checked'; }?>>販売停止中
+                                </label>
+                              </div>
                               <div class="col-4 edit"><a class="btn btn-green" href="update.php?menu_id=<?php echo $value_menu['id']; ?>">編集</a></div>
                             </div>
                           </div>
@@ -109,7 +115,7 @@ if (isset($_SESSION["USERID"])):
       </div>
     </section>
   </main>
-  <script src="../js/Sortable.js"></script>
+<script src="<?php echo $ctrl_js_path; ?>enabled.js" type="module"></script>
 <?php
 endif;
-require '../footer.php';
+require '../elements/footer.php';
