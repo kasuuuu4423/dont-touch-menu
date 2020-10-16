dd476f6bff4a65f8b0fa67324b009bb12d450574
kasuuuu4423 <info@shimizoo.com>
2020-10-16 20:00:31 +0900

    [add] ソートのajaxで変更機能

---
+++ bin/ajax/update_sort.php
@@ -0,0 +1,10 @@
+<?php
+require '../../lib/pdo/lib_pdo.php';
+
+$pdo = new Lib_pdo();
+
+$tbl = $_POST['tbl'];
+$item_id = $_POST['item_id'];
+$order = $_POST['order'];
+
+$pdo->update_order($tbl, $item_id, $order);
